099783c4a3900e85577b1e81216056b9e23eadb2
lclaudiomattoss <lclaudiomattoss@gmail.com>
2023-11-02 14:55:06 -0300

    Criação do Projeto "O Sambista" no XCode
    Criacao do projeto no XCode

---
+++ OSambista/OSambista/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OSambista
+//
+//  Created by Luiz Claudio Mattos da Silva on 02/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
